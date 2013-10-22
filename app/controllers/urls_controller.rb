43e146247d8134cedc9652cd71b60ba9eab6b20f
jforaker <jake@chalkable.com>
2013-10-22 14:59:26 -0400

    updating url_contr

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -2,29 +2,29 @@ class UrlsController < ApplicationController
 
   before_filter :authenticate_user!
 
-  def show
-    @url = Url.search_for(params[:hash_code])		
-  end
-
   def new
     @url = Url.new		
   end
 
   def create
-    hash = rand(100001232)
+    # Url(id: integer, link: string, hash_code: integer, created_at: datetime, updated_at: datetime)
+    hash = rand(8349862359)
     @url = Url.new
     @url.link = params[:link]
     @url.hash_code = hash
-    params[:hash_code] = hash
     if @url.save
-      redirect_to "/#{@url.hash_code}"
+      redirect_to "/#{@url.hash_code}"  #get '/:hash_code', to: 'urls#show'
     else
       render :new
     end  	
   end
 
+  def show
+    @url = Url.search_for(params[:hash_code])   
+  end
+
   def preview
   	@url = Url.get_link(params[:link])
-    redirect_to "http://" + @url.last.link
+    redirect_to "http://" + @url.last.link || "https://" + @url.last.link 
   end
 end
