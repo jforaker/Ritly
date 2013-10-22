443760c571f4c3b7bc336a6896bb6b14f6216848
jforaker <jake@chalkable.com>
2013-10-21 23:45:54 -0400

    updating preview cont

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,16 @@
 class Url < ActiveRecord::Base
+	
+	validates_presence_of :link, :message => "can't be empty"
+	validates :link, length: {
+	    minimum: 5,
+	    maximum: 100,
+	    tokenizer: lambda { |str| str.scan(//) },
+	    too_short: "link must be at least %{count} letters",
+	    too_long: "link must have at most %{count} letters"
+	}
+
+  	#validates :link, inclusion: { in: %w(/[www.(a-z)]/), message: "%{value} is not a valid url" }
+
 	def self.search_for(query)
 		where('hash_code LIKE :query', query: "%#{query}%")
 	end
