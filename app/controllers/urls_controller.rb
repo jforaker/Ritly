7cd37b3737fc91997311b3adb1509677038d0421
jforaker <jake@chalkable.com>
2013-11-04 18:31:09 -0500

    slicing change

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -10,8 +10,10 @@ class UrlsController < ApplicationController
     # Url(id: integer, link: string, hash_code: integer, created_at: datetime, updated_at: datetime)
     @url = Url.new
     @url.link = params[:link]
-    hash = rand(199999)
-    @url.hash_code = hash
+    hash = rand(12345678)
+    hasher = hash.to_s
+    new_hash = hasher.slice(0..5)
+    @url.hash_code = new_hash
     if @url.save
       redirect_to "/#{@url.hash_code}"  #get '/:hash_code', to: 'urls#show'
     else
