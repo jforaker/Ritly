06493c1e2095e30aaf4c061d28639afbe68ef84e
jforaker <jake@chalkable.com>
2013-11-04 18:42:40 -0500

    query change

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -10,7 +10,7 @@ class UrlsController < ApplicationController
     # Url(id: integer, link: string, hash_code: integer, created_at: datetime, updated_at: datetime)
     @url = Url.new
     @url.link = params[:link]
-    hash = rand(199999)
+    hash = rand(123456)
     @url.hash_code = hash
     if @url.save
       redirect_to "/#{@url.hash_code}"  #get '/:hash_code', to: 'urls#show'
